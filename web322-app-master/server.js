98fb75fb275b29e4b7d427392c876ed29c0459a3
Manav <aroramanav89@gmail.com>
2022-10-02 01:18:07 -0400

    initial commit

---
+++ web322-app-master/server.js
@@ -0,0 +1,26 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ___Manav___________________ Student ID: __153341219____________ Date: __02/10/2022______________
+*
+*  Online (Cyclic) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const dataService = require("./data-service.js");
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (_, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (_, res) => {
+  res.sendFIle(_dirname + "/views/about.html");
+});
+
+console.log(`Express http server listening on ${PORT}`);
