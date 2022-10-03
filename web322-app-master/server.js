7334176a265d575753a485fc4da8f9d86a122f7d
Manav <aroramanav89@gmail.com>
2022-10-02 22:58:25 -0400

    c

--- web322-app-master/server.js
+++ web322-app-master/server.js
@@ -9,14 +9,74 @@
 *
 ********************************************************************************/ 
 
-const express = require("express");
-const PORT = process.env.PORT || 8080;
-const dataService = require("./data-service.js");
-const app = express();
-app.use(express.static("public"));
-
-app.get("/", (_, res) => {
-  res.sendFile(__dirname + "/views/home.html");
-});
-
-console.log(`Express http server listening on ${PORT}`);
+var express = require("express");
+ var app = express();
+ var path = require("path");
+ 
+ //adding path tp product-service.js module to interact with it
+ var data = require("./product-service");
+ 
+ var HTTP_PORT = process.env.PORT || 8080;
+ 
+ function onHttpStart() {
+   console.log("Express http server listening on: " + HTTP_PORT);
+   return new Promise(function (res, req) {
+     data
+       .initialize()
+       .then(function (data) {
+         console.log(data);
+       })
+       .catch(function (err) {
+         console.log(err);
+       });
+   });
+ }
+ app.use(express.static("public"));
+ 
+ //setting up a defualt route for local host
+ app.get("/", function (req, res) {
+   res.sendFile(path.join(__dirname + "/views/index.html"));
+ });
+ 
+ //route to products
+ app.get("/products", function (req, res) {
+   data
+     .getPublishedProducts()
+     .then(function (data) {
+       res.json(data);
+     })
+     .catch(function (err) {
+       res.json({ message: err });
+     });
+ });
+ 
+ //route to demos
+ app.get("/demos", function (req, res) {
+   data
+     .getAllProducts()
+     .then(function (data) {
+       res.json(data);
+     })
+     .catch(function (err) {
+       res.json({ message: err });
+     });
+ });
+ 
+ //route to categories
+ app.get("/categories", function (req, res) {
+   data
+     .getCategories()
+     .then(function (data) {
+       res.json(data);
+     })
+     .catch(function (err) {
+       res.json({ message: err });
+     });
+ });
+ 
+ //if no route found show Page Not Found
+ app.use(function (req, res) {
+   res.status(404).sendFile(path.join(__dirname, "/views/error.html"));
+ });
+ 
+ app.listen(HTTP_PORT, onHttpStart)
